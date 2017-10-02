313df6cb87525a82aa02d787c60bec6d4d0b83f0
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-02 22:29:55 +0300

    Modified WebSecurityConfig.java to encrypt password using Bcrypt hash algorithm

--- src/main/java/fi/hh/Bookstore/WebSecurityConfig.java
+++ src/main/java/fi/hh/Bookstore/WebSecurityConfig.java
@@ -3,13 +3,21 @@ package fi.hh.Bookstore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import fi.hh.Bookstore.web.UserDetailService;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+    private UserDetailService userDetailsService;	
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,9 +36,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER").and()
-                .withUser("admin").password("password").roles("ADMIN");
+    	auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
