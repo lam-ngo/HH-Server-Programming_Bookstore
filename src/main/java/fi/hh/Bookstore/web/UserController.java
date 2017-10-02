e822013ed7b4fec4067e0bcb63bdb7fe49bd041c
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-02 22:18:56 +0300

    Added UserController for controlling user

---
+++ src/main/java/fi/hh/Bookstore/web/UserController.java
@@ -0,0 +1,60 @@
+package fi.hh.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+import fi.hh.Bookstore.domain.SignupForm;
+import fi.hh.Bookstore.domain.User;
+import fi.hh.Bookstore.domain.UserRepository;
+
+@Controller
+public class UserController {
+	@Autowired
+    private UserRepository userRepository; 
+	
+    @RequestMapping(value = "signup")
+    public String addStudent(Model model){
+    	model.addAttribute("signupform", new SignupForm());
+        return "signup";
+    }	
+    
+    @RequestMapping(value = "saveuser", method = RequestMethod.POST)
+    public String save(@Valid @ModelAttribute("signupform") SignupForm signupForm, BindingResult bindingResult) {
+    	if (!bindingResult.hasErrors()) { // validation errors
+    		if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match		
+	    		String pwd = signupForm.getPassword();
+		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+		    	String hashPwd = bc.encode(pwd);
+	
+		    	User newUser = new User();
+		    	newUser.setPasswordHash(hashPwd);
+		    	newUser.setUsername(signupForm.getUsername());
+		    	newUser.setRole("USER");
+		    	if (userRepository.findByUsername(signupForm.getUsername()) == null) { // Check if user exists
+		    		userRepository.save(newUser);
+		    	}
+		    	else {
+	    			bindingResult.rejectValue("username", "err.username", "Username already exists");    	
+	    			return "signup";		    		
+		    	}
+    		}
+    		else {
+    			bindingResult.rejectValue("passwordCheck", "err.passCheck", "Passwords does not match");    	
+    			return "signup";
+    		}
+    	}
+    	else {
+    		return "signup";
+    	}
+    	return "redirect:/login";    	
+    }    
+    
+}
