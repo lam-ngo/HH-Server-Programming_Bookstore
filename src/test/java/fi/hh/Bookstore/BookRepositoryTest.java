819f0bb2a60b0acb569701d404b7e7666a39f91e
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-03 09:48:22 +0300

    Added BookRepositoryTest.java to test the book repo

---
+++ src/test/java/fi/hh/Bookstore/BookRepositoryTest.java
@@ -0,0 +1,37 @@
+package fi.hh.Bookstore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.hh.Bookstore.domain.Category;
+import fi.hh.Bookstore.domain.Book;
+import fi.hh.Bookstore.domain.BookRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BookRepositoryTest {
+	@Autowired
+    private BookRepository repository;
+
+    @Test
+    public void findByLastnameShouldReturnStudent() {
+        List<Book> books = repository.findByTitle("Title1");
+        
+        assertThat(books).hasSize(1);
+        assertThat(books.get(0).getTitle()).isEqualTo("Title1");
+    }
+    
+    @Test
+    public void createNewBook() {
+    	Book book = new Book("Title1", "Author1", 2014, "ABC123", 10.5, new Category("BITE"));
+    	repository.save(book);
+    	assertThat(book.getId()).isNotNull();
+    }    
+}
